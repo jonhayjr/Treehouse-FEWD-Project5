cc252d681adab34d81cec228f36df3cba54ac41c
Jon Hay <jonhayjr@gmail.com>
2021-02-06 11:54:28 -0800

    Implemented image search logic

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 /*Run BaguetteBox Plugin*/
 baguetteBox.run('.gallery');
+
+const searchBar = document.querySelector('#search');
+
+function captionSearch() {
+    const thumbnail = document.querySelectorAll('a');
+    const searchInput = searchBar.value.toLowerCase();
+  
+    for (let i= 0; i < thumbnail.length; i++) {
+        let thumbnailCaption = thumbnail[i].getAttribute('data-caption');
+        if (thumbnailCaption.indexOf(searchInput) >= 0) {
+            thumbnail[i].style.display = "";
+        } else {
+            thumbnail[i].style.display = "none";
+        }
+    }
+}
+
+searchBar.addEventListener('keyup', captionSearch);
